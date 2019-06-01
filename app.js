438fe8fb056ef06f49a18c6f637c3c3b6ef59658
Puffycheeseses <noahheague@gmail.com>
2019-06-01 22:41:11 +1000

    Added server specific feature

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const pointer = require('./commands/pointer')
 // Set
 const client = new Discord.Client()
 Object.prototype.text = function() {return this.content.toLowerCase()} // Dry up some repeats
+let roles = ["584350364334292994", "584350519577935873", "584350659349053440"]
 
 // Run
 client.on('message', async msg => {
   if(/(^yo darnell$)/.test(msg.text())) msg.channel.send("yo")
   if (/(^yo darnell)( *|, *|. *)([a-zA-Z0-9])/.test(msg.text())) {
     await pointer.check(msg)
+  }
+  if(msg.channel.id === "582156871905968138" && msg.author.id === "159985870458322944") {
+    let player = await msg.guild.fetchMember(msg.content.split("GG ")[1].split(",")[0].replace(/\D/g,''))
+    let level = msg.content.split("level ")[1].split("!")[0]
+    if (level >= 10 && level < 20) {
+      console.log("level 10")
+      await player.removeRoles(roles)
+      await player.addRole("584350364334292994")
+    } else if (level >= 20 && level < 30) {
+      await player.removeRoles(roles)
+      await player.addRole("584350519577935873")
+    } else if (level >= 30 && level < 40) {
+      await player.removeRoles(roles)
+      await player.addRole("584350659349053440")
+    }
   }
 })
 
