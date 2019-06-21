735642592db3a55259ac6ed8a55a91b283b5d152
Puffycheeseses <noahheague@gmail.com>
2019-06-21 15:33:12 +1000

    refactor

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ const debug = require('debug')('Darnell:Main')
 const login = require('./private/private')
 const pointer = require('./commands/pointer')
 
-
 // Set
 const client = new Discord.Client()
-Object.prototype.text = function() {return this.content.toLowerCase()} // Dry up some repeats
+Discord.Message.prototype.text = function() {
+  return this.content.toLowerCase();
+};
 
 
-// Run
 client.on('message', async msg => {
-  if(/(^yo darnell$)/.test(msg.text())) msg.channel.send("yo")
-  if (/(^yo darnell)( *|, *|. *)([a-zA-Z0-9])/.test(msg.text())) {
-    await pointer.check(msg)
+  if(login.filter.some(word => msg.text().includes(word))) return pointer.deleteFilter.delete(msg);
+  if(/(^yo darne(l).*$)/.test(msg.text())) msg.channel.send("yo");
+  if (/(^yo darne(l).*)( *|, *|. *)([a-zA-Z0-9])/.test(msg.text())) {
+    await pointer.check(msg);
   }
 
   // Check if any roles need to be assigned
-  pointer.roles.check(msg)
-})
+  pointer.roles.check(msg);
+});
 
 client.on('ready', () => {
   debug(`Logged in as ${client.user.tag}!\nConnected to ${client.guilds.size} servers with ${client.users.size} users total`)
-})
+});
 
 client.login(login.getKey())
   .catch(e => debug(e))
